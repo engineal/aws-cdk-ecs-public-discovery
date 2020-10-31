2977909da6dfaaf5d2739cd1f303589b3edf48b7
Aaron Lucia <engineal@users.noreply.github.com>
2020-10-31 11:43:10 -0400

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface AwsCdkEcsPublicDiscoveryProps {
+  // Define construct properties here
+}
+
+export class AwsCdkEcsPublicDiscovery extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: AwsCdkEcsPublicDiscoveryProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
